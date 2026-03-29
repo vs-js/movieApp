afdcb409296b5795323d1a0f4e1d10179d115491
Varun <varunoshawa@gmail.com>
2026-03-28 20:20:42 -0400

    created the header component, deleted default icons and added the project icons and images

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    <div>App
+      <h1>Welcome to the Movie App</h1>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='wrapper'>
+        <header>
+            <h1>Find <span className='text-gradient'>Movies</span>You'll Enjoy Without the Hassle</h1>
+        </header>
+    </div>
+  )
+}
+
+export default Header
